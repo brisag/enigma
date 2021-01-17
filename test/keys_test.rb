d0dd93c0b0ee5129233e0259937ebbaaa1c482fd
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 00:20:07 -0700

    Add attributes and generate new keys

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -6,15 +6,26 @@ require 'minitest/pride'
 require './lib/keys'
 
 class KeyTest < MiniTest::Test
-    def test_it_exist_and_has_attributes
-      key = Key.new('0')
-      test_key = Key.new('02715')
+  def test_it_exist_and_has_attributes
+    key = Key.new('0')
+    test_key = Key.new('02715')
 
-      assert_instance_of Key, key
-      assert_instance_of Key, test_key
-      assert_equal '02715', test_key.key
-      assert_equal 0, test_key.a_key
-      assert_equal 0, test_key.b_key
-      assert_equal 0, test_key.c_key
-      assert_equal 0, test_key.d_key
-    end
+    assert_instance_of Key, key
+    assert_instance_of Key, test_key
+    assert_equal '02715', test_key.key
+    assert_equal 0, test_key.a_key
+    assert_equal 0, test_key.b_key
+    assert_equal 0, test_key.c_key
+    assert_equal 0, test_key.d_key
+  end
+
+  def test_it_can_create_keys
+    test_key = Key.new('02715')
+    test_key.create_keys
+
+    assert_equal 02, test_key.a_key
+    assert_equal 27, test_key.b_key
+    assert_equal 71, test_key.c_key
+    assert_equal 15, test_key.d_key
+  end
+end
