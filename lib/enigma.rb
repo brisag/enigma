6bf39a27d475f4971ebc7e5c1a372f5fe1a364f1
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 13:11:35 -0700

    Add initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,21 @@
 class Enigma
-	def initialize
-	end
+	attr_reader :message,
+							:key,
+							:date,
+							:alphabet,
+							:encoded_message
+
+ def initialize(message, key = Key.new, date = Offset.new)
+		@message = message.downcase.split("")
+		@key = key
+    @date = date
+    @alphabet = []
+		@encoded_message = []
+  end
+
+	def create_alphabet
+    @alphabet << (("a".."z").to_a << " ")
+    @alphabet.flatten!
+  end
+
 end
