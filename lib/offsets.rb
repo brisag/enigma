b339d7f1ee9b2e3c80bfff4d418d926f02af7732
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 00:30:38 -0700

    Add correct offset code

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,14 +1,23 @@
-class Key
-  attr_reader :key,
-              :a_key,
-              :b_key,
-              :c_key,
-              :d_key
-  def initialize(key)
-    @key   = key
-    @a_key = 0
-    @b_key = 0
-    @c_key = 0
-    @d_key = 0
+class Offset
+  attr_reader :date,
+              :a_offset,
+              :b_offset,
+              :c_offset,
+              :d_offset
+
+  def initialize(date)
+    @date     = date
+    @a_offset = 0
+    @b_offset = 0
+    @c_offset = 0
+    @d_offset = 0
   end
-end 
+
+  def create_offset
+    offset    = ((date.to_i)*(date.to_i)).to_s.split("")
+    @a_offset = offset[-4].to_i
+    @b_offset = offset[-3].to_i
+    @c_offset = offset[-2].to_i
+    @d_offset = offset.last.to_i
+  end
+end
