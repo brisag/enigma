c726cc4ec78a78d5b3f66ef1db5177853f1ceb96
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 00:26:41 -0700

    Add offset init

---
+++ lib/offsets.rb
@@ -0,0 +1,14 @@
+class Key
+  attr_reader :key,
+              :a_key,
+              :b_key,
+              :c_key,
+              :d_key
+  def initialize(key)
+    @key   = key
+    @a_key = 0
+    @b_key = 0
+    @c_key = 0
+    @d_key = 0
+  end
+end 
