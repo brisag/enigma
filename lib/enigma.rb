57cc566d9cdb83ebfab39454845fb0cf4bec0543
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 13:32:31 -0700

    Add foundation to create encryptor class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,17 +5,39 @@ class Enigma
 							:alphabet,
 							:encoded_message
 
- def initialize(message, key = Key.new, date = Offset.new)
-		@message = message.downcase.split("")
-		@key = key
-    @date = date
-    @alphabet = []
-		@encoded_message = []
+  def initialize
+    @encryptor = Encryptor.new
+    # @decryptor = Decryptor.new
   end
 
 	def create_alphabet
-    @alphabet << (("a".."z").to_a << " ")
-    @alphabet.flatten!
+	  @alphabet << (("a".."z").to_a << " ")
+	  @alphabet.flatten!
+	end
+
+  def build_encrypt(message, key, date)
+    encryption_hash = {
+									      encryption: message,
+									      key: key,
+									      date: date
+									    }
+    encryption_hash
   end
 
+  def encrypt(message, key = nil, date = nil)
+    build_encrypt(message, key || @encryptor.random_number_generator, date || @encryptor.todays_date_generator)
+  end
+
+	# def build_decrypt(message, key, date)
+	# 	encryption_hash = {
+	# 											decryption: message,
+	# 											key: key,
+	# 											date: date
+	# 										}
+	# 	decryption_hash
+	# end
+	#
+	# def encrypt(message, key = nil, date = nil)
+	# 	build_encrypt(message, key || decryption.random_number_generator, date || decryptio.todays_date_generator)
+	# end
 end
