d0e44a372c14eaf4efa05020a1874b9aa7897d82
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-18 13:46:21 -0700

    modify encryptor

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -6,7 +6,7 @@ class Encryptor
               :alphabet
 
   def initialize(message, key, date)
-    @message = message.downcase
+    @message = message
     @key = key
     @date = date
     @encoded_message = []
@@ -23,4 +23,36 @@ class Encryptor
     shift.generate
   end
 
+  def index_start
+    message.downcase.split("").map do |letter|
+      alphabet.find_index(letter)
+    end
+  end
+
+  def generate_encoded_message
+    index_array = index_start
+    shifts = get_shifts
+    create_return_hash(encode(index_array, shifts))
+  end
+
+  def encode(index_array, shifts)
+    encoded_message = []
+    index_array.each.with_index do |character, index|
+      if character == nil
+        encoded_message << message[index]
+        next
+      end
+      offs = character + shifts[index%4]
+      encoded_message << alphabet.rotate(offs).first
+    end
+    encoded_message.join
+  end
+
+  def create_return_hash(encrypted_string)
+    return_hash = {}
+    return_hash[:encryption] = encrypted_string
+    return_hash[:key] = key
+    return_hash[:date] = date
+    return_hash
+  end
 end
