8829b570554c01d656bb2f2de5897444e67ab792
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-18 16:44:56 -0700

    initialize enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,32 +3,34 @@ class Enigma
 							:key,
 							:date,
 							:alphabet,
-							:encoded_message
+							:encoded_message,
+							:decoded_message
 
-  def initialize
-    @encryptor = Encryptor.new
-    # @decryptor = Decryptor.new
-  end
-
-  def build_encrypt(message, key, date)
-    encryption_hash = {encryption: message,
-									      key: key,
-									      date: date}
-    encryption_hash
-  end
-
-  def encrypt(message, key = nil, date = nil)
-    build_encrypt(message, key || @encryptor.random_number_generator, date || @encryptor.todays_date_generator)
-  end
-
-	def build_decrypt(message, key, date)
-		encryption_hash = {decryption: message,
-												key: key,
-												date: date}
-		decryption_hash
+	def initialize()
 	end
 
-	def encrypt(message, key = nil, date = nil)
-		build_encrypt(message, key || decryption.random_number_generator, date || decryptio.todays_date_generator)
-	end
+
 end
+
+#   def build_encrypt(message, key, date)
+#     encryption_hash = {encryption: message,
+# 									      key: key,
+# 									      date: date}
+#     encryption_hash
+#   end
+#
+#   def encrypt(message, key = nil, date = nil)
+#     build_encrypt(message, key || @encryptor.random_number_generator, date || @encryptor.todays_date_generator)
+#   end
+#
+# 	def build_decrypt(message, key, date)
+# 		encryption_hash = {decryption: message,
+# 												key: key,
+# 												date: date}
+# 		decryption_hash
+# 	end
+#
+# 	def encrypt(message, key = nil, date = nil)
+# 		build_encrypt(message, key || decryption.random_number_generator, date || decryptio.todays_date_generator)
+# 	end
+# end
