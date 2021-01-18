e799007f0e9eeefdd3562e25ce820a0abe14b87e
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-17 19:30:19 -0700

    Add encryptor code

---
+++ lib/encryptor.rb
@@ -0,0 +1,28 @@
+
+class Encryptor
+  attr_reader :key,
+              :date,
+              :message,
+              :encoded_message,
+              :alphabet
+
+  def initialize(message, key, date)
+    @key = key
+    @date = date
+    @message = message
+    @encoded_message = []
+    @alphabet = ("a".."z").to_a << " "
+  end
+
+
+  def get_shifts
+    test_key = Key.new(key)
+    test_offset = Offset.new(date)
+    shift = Shift.new(test_key, test_offset)
+
+    test_key.create_keys
+    test_offset.create_offset
+    shift.generate
+  end
+
+end
