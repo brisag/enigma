0712188f285ebc398f27fc10545649588b932fa4
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-18 17:36:37 -0700

    test todays date and random key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -19,70 +19,38 @@ class EnigmaTest < MiniTest::Test
 		assert_instance_of Enigma, enigma
 	end
 
-	def test_it_can_encode
+  def test_it_can_encrypt_message_with_a_key_and_message
+    enigma = Enigma.new
+    expected = {encryption: 'keder ohulw',
+                key:        '02715',
+                date:       '040895'}
 
-		new_enigma = Enigma.new
-		#new_enigma.encrypt('hello world!', '02715", "040895")
-	  expected = {:encryption=>'keder ohulw!', :date=>'040895', :key=>'02715'}
-		assert_equal expected, new_enigma.encrypt('hello world!', '02715', '040895')
-	  expected = {:encryption=>'hxda', :date=>'190420', :key=>'01824'}
-		assert_equal expected, new_enigma.encrypt('abcd', '01824', '190420')
+    assert_equal expected, enigma.encrypt('hello world', '02715', '040895')
+  end
+
+	def test_it_can_decrypt_message_with_a_key_and_message
+    enigma = Enigma.new
+    expected = {decryption: 'hello world',
+                key:        '02715',
+                date:       '040895'}
+
+    assert_equal expected, enigma.decrypt('keder ohulw', '02715', '040895')
 	end
 
-	def test_it_encodes_single_characters
-		new_enigma = Enigma.new
-	  expected = {:encryption=>'d', :date=>'040895', :key=>'02715'}
-		assert_equal expected, new_enigma.encrypt('A', '02715', '040895')
-	end
-
-	def test_it_can_decrypt_with_key_and_date
-
+	def test_generate_random_key
+		skip
 		enigma = Enigma.new
-	  expected = {:decryption=>'hello world', :date=>'040895', :key=>'02715'}
-		assert_equal expected, enigma.decrypt('keder ohulw', '02715', '040895')
+
+		assert_equal '451291', enigma.generate_key
 	end
 
-	def test_it_can_decrypt_a_message_using_todays_date
-		Date.stubs(:strftime).returns(Date.new(2021,1,18))
-	  enigma = Enigma.new
-	  encrypted = enigma.encrypt('hello world', '02715')
-	  expected = {:decryption=>'hello world', :date=>'180121', :key=>'02715'}
-	  assert_equal expected, enigma.decrypt(encrypted[:encryption], '02715')
+	def test_generate_date_today
+		enigma = Enigma.new
+
+		assert_equal '180121', enigma.todays_date
 	end
 
-	def test_it_can_encrypt
-	  enigma = Enigma.new
-	  expected = {encryption: 'vjqtbeaweqihssi', key: '08304', date: '291018'}
-	  assert_equal expected, enigma.encrypt('hello world end', '08304', '291018')
-	end
 
-	def test_encrypt_a_message_with_a_key_using_todays_date
-	  Date.stubs(:today).returns(Date.new(1995,8,4))
-	  enigma = Enigma.new
-	  expected = ({:encryption=>'keder ohulw', :date=>'040895', :key=>'02715'})
-	  assert_equal expected, enigma.encrypt('hello world', '02715')
-	end
-
-	def test_encrypt_a_message_with_a_message_and_no_key_or_date
-	  Date.stubs(:today).returns(Date.new(1995,8,4))
-	  enigma = Enigma.new
-	  enigma.stubs(:generate_random_key).returns('02715')
-	  expected = ({:encryption=>'keder ohulw', :date=>'040895', :key=>'02715'})
-	  assert_equal expected, enigma.encrypt('hello world')
-	end
-
-	def test_it_can_encrypt_message_with_characters_outside_set
-		new_enigma = Enigma.new
-		expected = {:encryption=>'!@', :date=>'040895', :key=>'02715'}
-		assert_equal expected, new_enigma.encrypt('!@', '02715', '040895')
-	end
-
-	def test_it_can_crack_an_encryption_with_a_date
-	  skip
-	  enigma = Enigma.new
-	  expected = {decryption: 'hello world end',date: '291018',key: '08304'}
-	  assert_equal expected, enigma.crack('vjqtbeaweqihssi', '291018')
-	end
 end
 
 
