f8a59ee26ac275a975460567f900d87683548b96
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-18 17:37:06 -0700

    create encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,13 +3,28 @@ class Enigma
 							:key,
 							:date,
 							:alphabet,
-							:encoded_message,
-							:decoded_message
+							:encoded_message
 
 	def initialize()
 	end
 
+  def encrypt(message, key = Key.new, date = Offset.new)
+		encryptor = Encryptor.new(message, key, date)
+		encryptor.generate_encoded_message
+	end
 
+	def decrypt(message, key = Key.new, date = Offset.new)
+		decryptor = Decryptor.new(message, key, date)
+		decryptor.generate_decoded_message
+	end
+
+  def generate_key
+    rand(1000000).to_s.rjust(5,'0')
+  end
+
+	def todays_date
+		Date.today.strftime("%d%m%y")
+	end
 end
 
 #   def build_encrypt(message, key, date)
@@ -19,11 +34,11 @@ end
 #     encryption_hash
 #   end
 #
-#   def encrypt(message, key = nil, date = nil)
-#     build_encrypt(message, key || @encryptor.random_number_generator, date || @encryptor.todays_date_generator)
+#   def encrypt(message, key = generate_key, date = todays_date)
+#     build_encrypt(message, key, date)
 #   end
-#
-# 	def build_decrypt(message, key, date)
+# #
+# # 	def build_decrypt(message, key, date)
 # 		encryption_hash = {decryption: message,
 # 												key: key,
 # 												date: date}
