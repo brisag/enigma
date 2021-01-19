0a7a1e9767a75d26e7ac13112474b62c73159c74
Brisa Garcia <brisagarciaglz@gmail.com>
2021-01-18 19:11:26 -0700

    Update enigma class with methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,19 +1,13 @@
 class Enigma
-	attr_reader :message,
-							:key,
-							:date,
-							:alphabet,
-							:encoded_message
-
 	def initialize()
 	end
 
-  def encrypt(message, key = Key.new, date = Offset.new)
+  def encrypt(message, key = generate_key, date = todays_date)
 		encryptor = Encryptor.new(message, key, date)
 		encryptor.generate_encoded_message
 	end
 
-	def decrypt(message, key = Key.new, date = Offset.new)
+	def decrypt(message, key = generate_key, date = todays_date)
 		decryptor = Decryptor.new(message, key, date)
 		decryptor.generate_decoded_message
 	end
@@ -21,31 +15,8 @@ class Enigma
   def generate_key
     rand(1000000).to_s.rjust(5,'0')
   end
-
+	
 	def todays_date
 		Date.today.strftime("%d%m%y")
 	end
 end
-
-#   def build_encrypt(message, key, date)
-#     encryption_hash = {encryption: message,
-# 									      key: key,
-# 									      date: date}
-#     encryption_hash
-#   end
-#
-#   def encrypt(message, key = generate_key, date = todays_date)
-#     build_encrypt(message, key, date)
-#   end
-# #
-# # 	def build_decrypt(message, key, date)
-# 		encryption_hash = {decryption: message,
-# 												key: key,
-# 												date: date}
-# 		decryption_hash
-# 	end
-#
-# 	def encrypt(message, key = nil, date = nil)
-# 		build_encrypt(message, key || decryption.random_number_generator, date || decryptio.todays_date_generator)
-# 	end
-# end
